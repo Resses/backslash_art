f6e56a950ca4c33e44913065c824c4a320c721cf
Renee Esses <reneeesses@gmail.com>
2018-12-28 12:25:30 -0500

    Cleaned up code

--- KinectProject/KinectTutorial/src/app.h
+++ KinectProject/KinectTutorial/src/app.h
@@ -12,7 +12,6 @@
 #include <string>
 
 #include <Kinect.h>
-#include <Kinect.Face.h>
 #include <Kinect.VisualGestureBuilder.h>
 #include <atlbase.h>
 
@@ -36,49 +35,24 @@ typedef long long int64;
 template<typename T>
 void SafeRelease(T& ptr) { if(ptr) { ptr->Release(); ptr = nullptr; } }
 
-class App
-{
+class App {
 public:
 	void Init();
 	void Tick(float deltaTime);
-	void initializeGesture(HRESULT hr);
-	void orderPeople(HRESULT hr);
-	void sendMessage(std::string message_name, int message_value, int body_index, std::string print_message); 
-	void sendMessage(std::string message_name, int message_value, int body_index);
-	void updateGestureFrame(HRESULT hr, int count);
-	void updateLean(HRESULT hr, IBody* body, int bodyIndex);
-	void updateArmRaise(HRESULT hr, IBody* body, int bodyIndex);
-	void updateHandState(HRESULT hr, IBody* body, int bodyIndex);
-	void result(const CComPtr<IVisualGestureBuilderFrame>& gestureFrame, const CComPtr<IGesture>& gesture, const int count, int i);
 	void Shutdown();
-
 	void SetPixelBuffer(uint32* pixelBuffer) { m_pixelBuffer = pixelBuffer; }
 	//safe way of plotting a pixel
-	void Plot(int x, int y, uint32 color)
-	{
+	void Plot(int x, int y, uint32 color) {
 		if (x < 0 || x >= SCRWIDTH || y < 0 || y >= SCRHEIGHT)
 			return;
 		m_pixelBuffer[x + y * SCRWIDTH] = color;
 	}
-	inline std::wstring trim(const std::wstring& str)
-	{
+	inline std::wstring trim(const std::wstring& str) {
 		const std::wstring::size_type last = str.find_last_not_of(L" ");
 		if (last == std::wstring::npos) {
 			throw std::runtime_error("failed ");
 		}
 		return str.substr(0, last + 1);
-	}
-	double GetCurrentSeconds() {
-		time_t timer;
-		struct tm y2k = { 0 };
-		double seconds;
-
-		y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
-		y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
-
-		time(&timer);  /* get current time; same as: timer = time(NULL)  */
-
-		return difftime(timer, mktime(&y2k));
 	}
 
 private:
@@ -86,33 +60,38 @@ private:
 	//pointer to buffer that contains pixels that get pushed to the screen
 	//size of this buffer is SCRWIDTH * SCRWIDTH * sizeof(uint32)
 	uint32* m_pixelBuffer = nullptr;
+	uint32* m_colorBuffer = nullptr;
 
+	// kinect sensor
 	IKinectSensor* m_sensor = nullptr;
+
+	// frame readers
 	IColorFrameReader* m_colorFrameReader = nullptr;
 	IBodyFrameReader* m_bodyFrameReader = nullptr;
-	IBody* m_bodies[BODY_COUNT] = { 0 };
-	BOOLEAN handraised = false;
-	BOOLEAN on_off = false;
-	BOOLEAN hand_closed = false;
-	BOOLEAN hand_open = false;
-	BOOLEAN hand_switch = false;
-	bool lean[4] = { 0 };
-	IFaceFrameReader* m_faceFrameReaders[BODY_COUNT] = { 0 };
-	IFaceFrameSource* m_faceFrameSources[BODY_COUNT] = { 0 };
 	IVisualGestureBuilderFrameReader* gestureFrameReader[BODY_COUNT] = { 0 };
+
+	// gesture booleans
+	IBody* m_bodies[BODY_COUNT] = { 0 };
+	bool handraised[BODY_COUNT] = { 0 };
+	bool hand_closed[BODY_COUNT] = { 0 };
+	bool hand_open[BODY_COUNT] = { 0 };
+	bool lean[BODY_COUNT][4] = { 0 }; // 4 lean directions (left, right, forward, back)
+
 	std::vector<CComPtr<IGesture>> gestures;
-	std::vector<BOOLEAN> bool_gestures[BODY_COUNT];
-	bool summon[4] = { 0 }; 
-	int summoncounter[4] = { 0 };
-	//int hipscounter[BODY_COUNT] = { 0 };
-	//forward left = 0, back left = 1, forward right = 2, back right = 3
-	//speakers:
-	//Forward left = 1
-	// forward right = 2
-	// back right = 3
-	// back left = 4
-	uint32* m_colorBuffer = nullptr;
+	std::vector<bool> bool_gestures[BODY_COUNT];
 	double timeOfLastGesture[BODY_COUNT] = { 0 }; 
 
+	// METHODS
+	void initializeGesture();
+	void orderPeople();
+	double GetCurrentSeconds();
+
+	void updateLean(IBody* body, int bodyIndex);
+	void updateArmRaise(IBody* body, int bodyIndex);
+	void updateHandState(IBody* body, int bodyIndex);
+	void updateCustomGestures(int count);
+	void updateGestureHelper(const CComPtr<IVisualGestureBuilderFrame>& gestureFrame, const CComPtr<IGesture>& gesture, const int count, int i);
+
+	void sendMessage(std::string message_name, int message_value, int body_index, std::string print_message = ""); 
 	
 };
